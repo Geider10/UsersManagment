9b3791f63f85fc2fb25f708ea97cd715711d387a
Geider Frias <geiderde20@gmail.com>
2025-04-03 22:28:56 -0300

    feat: agrega el modelo de persona

---
+++ AuthManagment_WebAPI_80/Models/PersonaModel.cs
@@ -0,0 +1,19 @@
+﻿namespace AuthManagment_WebAPI_80.Models
+{
+    public class PersonaModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role Rol { get; set; }
+        public DateTime DateCreation { get; set; }
+
+
+    }
+
+    public enum Role
+    {
+        Admin, User
+    }
+}
