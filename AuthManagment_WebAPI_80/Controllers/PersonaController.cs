cc0f8a1db45ea364f5a7f224b022d68052e384d1
Geider Frias <geiderde20@gmail.com>
2025-04-04 14:56:35 -0300

    feat: agregar la API persona getAll, getById, update, delete

--- AuthManagment_WebAPI_80/Controllers/PersonaController.cs
+++ AuthManagment_WebAPI_80/Controllers/PersonaController.cs
@@ -29,5 +29,57 @@ namespace AuthManagment_WebAPI_80.Controllers
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(Create), new { id = persona.Id }, persona);
         }
+        [HttpGet]
+        [Route("get")]
+        public async Task<ActionResult> GetAll()
+        {
+            var personas = await _context.Personas.ToListAsync();
+            if(personas == null)
+            {
+                return NotFound("No personas found");
+            }
+            return Ok(personas);
+        }
+        [HttpGet]
+        [Route("get/{id}")]
+        public async Task<ActionResult> GetById([FromRoute] string id)
+        {
+            var persona = await _context.Personas.FindAsync(id);
+            if (persona == null)
+            {
+                return NotFound("Persona no found");
+            }
+            return Ok(persona);
+        }
+        [HttpPut][Route("update/{idPersona}")]
+        public async Task<ActionResult> Update([FromRoute] string idPersona, [FromBody] PersonaModel persona)
+        {
+            var personaDB = await _context.Personas.FindAsync(idPersona);
+            if(personaDB == null)
+            {
+                return NotFound("Persona not found");
+            } 
+            personaDB.Name = persona.Name;
+            personaDB.Email = persona.Email;
+            personaDB.Password = persona.Password;
+
+            await _context.SaveChangesAsync();
+            return Ok("Persona Updated");
+
+        }
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public async Task<ActionResult> Delete([FromRoute] string id)
+        {
+            var persona = await _context.Personas.FindAsync(id);
+            if(persona == null)
+            {
+                return NotFound("Persona not found");
+            }
+            _context.Personas.Remove(persona);
+            await _context.SaveChangesAsync();
+
+            return Ok("Persona deleted");
+        }
     }
 }
