da54bb6928e68cd2ffc13988b40c121d533949b4
Geider Frias <geiderde20@gmail.com>
2025-04-05 23:53:40 -0300

    refactor(personaController): agrega el DTO de persona

--- AuthManagment_WebAPI_80/Controllers/PersonaController.cs
+++ AuthManagment_WebAPI_80/Controllers/PersonaController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using AuthManagment_WebAPI_80.Data;
 using AuthManagment_WebAPI_80.Models;
+using AuthManagment_WebAPI_80.DTOs;
 
 namespace AuthManagment_WebAPI_80.Controllers
 {
@@ -18,52 +19,67 @@ namespace AuthManagment_WebAPI_80.Controllers
 
         [HttpPost]
         [Route("create")]
-        public async Task<ActionResult> Create([FromBody] PersonaModel persona)
+        public async Task<ActionResult> Create([FromBody] PersonaDTO personaDTO)
         {
-            Console.WriteLine(persona);
-            if (persona == null)
+            if (personaDTO == null) return BadRequest("Persona is null");
+            var persona = new PersonaModel()
             {
-                return BadRequest("Persona is null");
-            }
-            await _context.Personas.AddAsync(persona);
+                Id = "111",
+                Name = personaDTO.Name,
+                Email = personaDTO.Email,
+                Password = personaDTO.Password,
+                Rol = Role.User,
+            };
+  
+            _context.Personas.Add(persona);
             await _context.SaveChangesAsync();
+
             return CreatedAtAction(nameof(Create), new { id = persona.Id }, persona);
         }
         [HttpGet]
         [Route("get")]
         public async Task<ActionResult> GetAll()
         {
-            var personas = await _context.Personas.ToListAsync();
-            if(personas == null)
+            var listaDTO = new List<PersonaDTO>();
+            var listaDB = await _context.Personas.ToListAsync();
+
+            if(listaDB == null) return NotFound("No personas found");
+            foreach (var item in listaDB)
             {
-                return NotFound("No personas found");
+                listaDTO.Add(new PersonaDTO
+                {
+                    Name = item.Name,
+                    Email = item.Email,
+                });
             }
-            return Ok(personas);
+
+            return Ok(listaDTO);
         }
         [HttpGet]
         [Route("get/{id}")]
         public async Task<ActionResult> GetById([FromRoute] string id)
         {
-            var persona = await _context.Personas.FindAsync(id);
-            if (persona == null)
-            {
-                return NotFound("Persona no found");
-            }
-            return Ok(persona);
+            var personaDTO = new PersonaDTO();
+            var personaDB = await _context.Personas.FindAsync(id);
+            if (personaDB == null) return NotFound("Persona no found");
+
+            personaDTO.Name = personaDB.Name;
+            personaDTO.Email = personaDB.Email;
+
+            return Ok(personaDTO);
         }
         [HttpPut][Route("update/{idPersona}")]
-        public async Task<ActionResult> Update([FromRoute] string idPersona, [FromBody] PersonaModel persona)
+        public async Task<ActionResult> Update([FromRoute] string idPersona, [FromBody] PersonaDTO personaDTO)
         {
             var personaDB = await _context.Personas.FindAsync(idPersona);
-            if(personaDB == null)
-            {
-                return NotFound("Persona not found");
-            } 
-            personaDB.Name = persona.Name;
-            personaDB.Email = persona.Email;
-            personaDB.Password = persona.Password;
+            if(personaDB == null) return NotFound("Persona not found");
 
+            personaDB.Name = personaDTO.Name;
+            personaDB.Email = personaDTO.Email;
+
+            _context.Personas.Update(personaDB);    
             await _context.SaveChangesAsync();
+
             return Ok("Persona Updated");
 
         }
@@ -72,10 +88,8 @@ namespace AuthManagment_WebAPI_80.Controllers
         public async Task<ActionResult> Delete([FromRoute] string id)
         {
             var persona = await _context.Personas.FindAsync(id);
-            if(persona == null)
-            {
-                return NotFound("Persona not found");
-            }
+            if(persona == null) return NotFound("Persona not found");
+
             _context.Personas.Remove(persona);
             await _context.SaveChangesAsync();
 
